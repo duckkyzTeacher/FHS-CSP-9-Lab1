bec3715d79613c9358c156dd57ec38c4550bb6c5
Benjamin Johnson <bjohnson@fcusd.org>
2024-11-06 09:11:33 -0800

    Updated problem numbers

--- functionsLabPartA.py
+++ functionsLabPartA.py
@@ -7,7 +7,7 @@
 
 
 
-# Problem 2: Scope of Variables
+# Problem 2-3: Scope of Variables
 # Write a function called 'outer' that declares a variable 'x' inside it, sets it to 10, and prints it.
 
 
@@ -16,7 +16,7 @@
 
 
 
-# Problem 3: Conditional Return Values
+# Problem 4: Conditional Return Values
 # Write a function called 'divisible_by_3' that takes an integer as a parameter.
 # The function should return True if the number is divisible by 3, otherwise return False.
 # Test the function by calling it with different numbers.
@@ -26,7 +26,7 @@
 
 
 
-# Problem 4: Multiple Return Values
+# Problem 5: Multiple Return Values
 # Create a function called min_max that takes a list of numbers as a parameter and returns both the smallest and largest numbers. 
 # Test the function by printing both the minimum and maximum.
 
--- functionsLabPartB.py
+++ functionsLabPartB.py
@@ -1,4 +1,4 @@
-# Problem 5: Using Default Parameters
+# Problem 1: Using Default Parameters
 # Write a function called 'introduce_yourself' that takes two parameters: 'name' and 'age'.
 # The 'age' parameter should have a default value of 18.
 # The function should print "Hello, my name is {name} and I am {age} years old." 
@@ -8,7 +8,7 @@
 
 
 
-# Problem 6: Variable-Length Arguments (*args) and multiple returns
+# Problem 2: Variable-Length Arguments (*args) and multiple returns
 # Write a function called 'sum_avg_of_numbers' that accepts a variable number of arguments using *args.
 # The function should return the sum and average of all the numbers passed to it.
 # Test the function with different numbers of arguments.
@@ -18,7 +18,7 @@
 
 
 
-# Problem 7: Variable-Length Arguments (*args) with Keyword argument
+# Problem 3: Variable-Length Arguments (*args) with Keyword argument
 # Write a function called 'combine_strings' that takes a separator string and an arbitrary number 
 # of strings using *args. The function should return a single string combining all the input 
 # strings, separated by the separator. (No seperator at the start or end
